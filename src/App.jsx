a8f7fe7d68648050fa692c442609ed6a90a48496
Luca Pallagi <luca.pallagi@stud.kbw.ch>
2026-02-18 10:49:34 +0100

    Lade HashRouter hoch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
 import FrontPage from "./FrontPage.jsx"
+import { HashRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-      <FrontPage></FrontPage>
+        <HashRouter>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<FrontPage />} />
+            </Routes>
+        </HashRouter>
     </>
   )
 }
--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -7,11 +7,12 @@ function FrontPage(){
             <div id="FrontPage">
                 <img src={profilePicture} alt={"ProfilePicture"} id="ProfilePicture"/>
                 <div id="FP-AboutMe">
+                    <p id={"myName"}>Luca Pallagi</p>
                     Ein wenig über mich
                     <p>
                         Ich bin in der Ausbildung als Applikationsentwickler in der Informatikmittelschule Buelrain.
                         In meiner Freizeit programmiere ich gerne kleine Projekte, gehe ins Fitnessstudio und bouldere gerne in der Halle.
-                        Meine Projekte sind meistens in React geschrieben, welche ich am besten beherrsche.
+                        Meine Projekte sind meistens in React geschrieben, das ich am besten beherrsche.
                     </p>
                 </div>
             </div>
