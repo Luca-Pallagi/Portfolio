d3472b29ebca78e7daccdf24c3c170908a2d2ff5
Luca Pallagi <luca.pallagi@stud.kbw.ch>
2026-02-18 09:53:59 +0100

    Erstelle ueber mich

--- src/FrontPage.jsx
+++ src/FrontPage.jsx
@@ -6,6 +6,14 @@ function FrontPage(){
         <>
             <div id="FrontPage">
                 <img src={profilePicture} alt={"ProfilePicture"} id="ProfilePicture"/>
+                <div id="FP-AboutMe">
+                    Ein wenig über mich
+                    <p>
+                        Ich bin in der Ausbildung als Applikationsentwickler in der Informatikmittelschule Buelrain.
+                        In meiner Freizeit programmiere ich gerne kleine Projekte, gehe ins Fitnessstudio und bouldere gerne in der Halle.
+                        Meine Projekte sind meistens in React geschrieben, welche ich am besten beherrsche.
+                    </p>
+                </div>
             </div>
         </>
     )
