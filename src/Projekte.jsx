9529d44fba3145e615c7f6ce1a8df31391d223bd
Luca Pallagi <luca.pallagi@stud.kbw.ch>
2026-03-04 10:56:25 +0100

    Erstellen eines Projektes

--- src/Projekte.jsx
+++ src/Projekte.jsx
@@ -1,5 +1,6 @@
 import quizShow from "./assets/quizShow.png";
 import gambling from "./assets/gambling.png";
+import tictactoe from "./assets/tictactoe.png";
 import './App.css';
 
 function Projekte(){
@@ -12,6 +13,14 @@ function Projekte(){
                     <div className="project-card-content">
                         <h3>Quiz Show</h3>
                         <p>In der Quiz Show wird man je nach Ausgewählten Kategorien Fragen beantworten müssen.</p>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <img alt="Tic Tac Toe" src={tictactoe}/>
+                    <div className="project-card-content">
+                        <h3>Tic Tac Toe</h3>
+                        <p>In diesem TicTacToe gibt es drei Schwierigkeitsgrade und einem Modi mit Spieler gegen
+                            Spieler</p>
                     </div>
                 </div>
                 <div className="project-card">
