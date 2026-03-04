b156297869009295efb59be8e9ac0579bb586052
Luca Pallagi <luca.pallagi@stud.kbw.ch>
2026-03-04 10:45:57 +0100

    Erstelle zwei Projekte und CSS dazu

--- src/Projekte.jsx
+++ src/Projekte.jsx
@@ -1,11 +1,26 @@
-
+import quizShow from "./assets/quizShow.png";
+import gambling from "./assets/gambling.png";
+import './App.css';
 
 function Projekte(){
 
     return(
         <>
             <div id="meineProjekte">
-
+                <div className="project-card">
+                    <img alt="Quiz Show" src={quizShow}/>
+                    <div className="project-card-content">
+                        <h3>Quiz Show</h3>
+                        <p>In der Quiz Show wird man je nach Ausgewählten Kategorien Fragen beantworten müssen.</p>
+                    </div>
+                </div>
+                <div className="project-card">
+                    <img alt="Gambling Game" src={gambling}/>
+                    <div className="project-card-content">
+                        <h3>Gambling Game</h3>
+                        <p>In diesem Spiel kann man mit einer Slot Machine einfach fiktives Geld verdienen.</p>
+                    </div>
+                </div>
             </div>
         </>
     )
